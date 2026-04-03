0b39d04ba60ae2bf1d7a4a6d3b56dc7d1b55a9dd
Luminus <nurudeenexpert@gmai.com>
2026-04-03 15:07:56 +0100

    added minus functionality to minus button

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ document.querySelector('.js-add-button').addEventListener("click", () => {
     quantity += 1;
     document.querySelector('.quantity-view').innerText = quantity;
 });
+document.querySelector('.js-minus-button').addEventListener("click", () => {
+    if (quantity === 0) {
+        return
+    } else {
+        quantity = quantity - 1;
+    document.querySelector('.quantity-view').innerText = quantity;
+    }
+});
